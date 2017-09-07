91a77b78ba09488c550ef1fd60240c3f53d35b47
Aakash Ahmed <aakashahmed18@gmail.com>
2017-09-07 18:48:56 +0500

    Configured for it's domain

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "Printex Shopify App"
   config.api_key = "d9f3d508e72f69020b0e1ce12366e11b"
   config.secret = "7a978cc885a34979a35cc6d2f73db241"
   config.scope = "read_orders, write_orders, read_products, write_products, read_script_tags, write_script_tags, read_content, write_content"
@@ -8,7 +8,7 @@ ShopifyApp.configure do |config|
   config.session_repository = Shop
   config.after_authenticate_job = { job: Shopify::AfterAuthenticateJob, inline: false }
   config.webhooks = [
-    {topic: 'orders/paid', address: 'https://96f634e2.ngrok.io/webhooks/order/create', format: 'json'}
+    {topic: 'orders/paid', address: 'https://shopify.printexinc.com/webhooks/order/create', format: 'json'}
     # {topic: 'app/uninstalled', address: 'https://24e3843d.ngrok.io/webhooks/uninstall/', format: 'json'}
   ]
 end
